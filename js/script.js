5d6cf85de3fb017e5bacdfad5b65ef9a87dceace
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2021-04-09 11:28:40 +0000

    Added getTextInfo function at script.js

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// Function to Get Text Information (including Invalid Input)
+function getTextInfo() {
+    // Set Up Variable
+    let txt="";
+    // Get the value of the input field
+    let myText = document.getElementById("mytext").value;
+    // Check if Text is Valid
+    if (myText.toString().length == 0) {
+        txt += `Invalid Input.  Text cannot be empty.`;
+    } else {
+        // Get Text Information
+        txt += `You have entered the text ${myText}.`;
+        txt += `The length of the longest palindromic subsequence is ${lps(myText)}.`;
+    }
+}
